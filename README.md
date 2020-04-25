a1c72741b0f8ef2ad961914025735d95966c6c92
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2020-04-25 17:13:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pkg_jtfavorites
+Pakage to manage a list of favorites for plugin/module action.<br />
+The list of favorites can be output to different positions (cpanel, menu, icon) via an admin module.
---
+++ src/plugins/system/jtfavorites/jtfavorites.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * @package      Joomla.Plugin
+ * @subpackage   System.Jtfavorites
+ *
+ * @author       Guido De Gobbis <support@joomtools.de>
+ * @copyright    2020 JoomTools.de - All rights reserved.
+ * @license      GNU General Public License version 3 or later
+ **/
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Factory;
+use Joomla\CMS\User\User;
+
+/**
+ * System plugin to manage a list of favorites for plugin/module action.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class PlgSystemJtfavorites extends CMSPlugin
+{
+	/**
+	 * @var     JApplicationCms
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected $app = null;
+
+	/**
+	 * Database object.
+	 *
+	 * @var     JDatabaseDriver
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected $db;
+
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var     boolean
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Adds additional fields to plugins/modules editing form to activate as favorite
+	 *
+	 * @param Form  $form The form to be altered.
+	 * @param mixed $data The associated data for the form.
+	 *
+	 * @return   boolean
+	 *
+	 * @throws   \Exception
+	 * @since    __DEPLOY_VERSION__
+	 */
+	public function onContentPrepareForm($form, $data)
+	{
+		if (!isset($data->params))
+		{
+			return true;
+		}
+
+		if (!$form instanceof Form)
+		{
+			$this->_subject->setError('JERROR_NOT_A_FORM');
+
+			return false;
+		}
+
+		$formName = $form->getName();
+
+		$allowedFormNames = array(
+			'com_plugins.plugin',
+			'com_modules.module',
+			'com_modules.module.admin',
+		);
+
+		if (!in_array($formName, $allowedFormNames))
+		{
+			return true;
+		}
+
+		/**
+		 * We only allow users who has Super User permission change this setting for himself or for other users
+		 * who has same Super User permission
+		 */
+
+		$user = Factory::getUser();
+
+		if (!$user->authorise('core.edit'))
+		{
+			return true;
+		}
+
+		// If we are on the save command, no data is passed to $data variable, we need to get it directly from request
+		$jformData = $this->app->input->get('jform', array(), 'array');
+
+		if ($jformData && !$data)
+		{
+			$data = $jformData;
+		}
+
+		if (is_array($data))
+		{
+			$data = (object) $data;
+		}
+
+		Form::addFormPath(dirname(__FILE__) . '/forms');
+
+		if (version_compare(JVERSION, '3.10', 'ge'))
+		{
+			$form->loadFile('jtfavorites.3.10');
+
+			$fields = array(
+				'add_to_favoites',
+				'favorite_title_note',
+				'favorite_title',
+			);
+
+			$this->app->setUserState($formName . '.edit.global.fields', $fields);
+
+			return true;
+		}
+
+		$form->loadFile('jtfavorites');
+
+		return true;
+	}
+
+	/**
+	 * Method is called when an extension is being saved
+	 *
+	 * @param string  $context The extension
+	 * @param JTable  $table   DataBase Table object
+	 * @param boolean $isNew   If the extension is new or not
+	 * @param array   $params  Extension params
+	 *
+	 * @return   void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onExtensionAfterSave($context, $table, $isNew, $params)
+	{
+		return null;
+		// Called after extension is saved successful
+		// TODO update entry in table
+	}
+
+	/**
+	 * Method is called when an extension is being deleted from trash
+	 *
+	 * @param string $context The extension
+	 * @param JTable $table   DataBase Table object
+	 *
+	 * @return   void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onExtensionAfterDelete($context, $table)
+	{
+		return null;
+		// Called after deleted in trash
+		// TODO clear entry in table
+	}
+}
