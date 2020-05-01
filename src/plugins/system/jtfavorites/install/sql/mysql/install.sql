ac6c890559dce597d68cb0c7586e8be7e0fde682
Tobias Zulauf <zero-24@users.noreply.github.com>
2020-05-01 12:28:17 +0200

    Update src/plugins/system/jtfavorites/install/sql/mysql/install.sql
    Co-authored-by: Guido De Gobbis <10517822+degobbis@users.noreply.github.com>

--- src/plugins/system/jtfavorites/install/sql/mysql/install.sql
+++ src/plugins/system/jtfavorites/install/sql/mysql/install.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS `#__jtfavorites` (
   `extension_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `name` varchar(100),
-  PRIMARY KEY (`extension_id`, `user_id`),
+  PRIMARY KEY (`extension_id`, `user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
