7b003905151b0569a8c66f122ec0642d23c8124b
Guido De Gobbis <guido@degobbis.de>
2020-05-02 14:32:42 +0200

    Überarbeitung der Datenbanktabelle
    - Änderung der Bezeichnungen
    - Änderung der Indexes
    - Hinzufügen der <code>client_id</code> zur Unterscheidung zwischen Site und Administrator Module für die gruppierte Ausgabe

--- src/plugins/system/jtfavorites/install/sql/mysql/install.sql
+++ src/plugins/system/jtfavorites/install/sql/mysql/install.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `#__jtfavorites` (
-  `extension_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `name` varchar(100),
-  PRIMARY KEY (`extension_id`, `user_id`)
+  `assets_name` varchar(50) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `favorite_title` varchar(100),
+  `state` tinyint(1) NOT NULL,
+  PRIMARY KEY (`user_id`, `assets_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
